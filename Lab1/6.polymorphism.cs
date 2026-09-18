378a0a493346e76dd4346b93f06c143a3e322525
DariaPoliakovaa <dashacovic@gmail.com>
2026-09-18 11:24:24 +0300

    fix polymorphism example

--- Lab1/6.polymorphism.cs
+++ Lab1/6.polymorphism.cs
@@ -13,14 +13,18 @@ public class Lesson
 
 
 // З дотриманням принципу Polymorphism
-public class Lesson
+public abstract class LessonType
 {
-    public string Type { get; set; } // "Lecture" або "Lab"
-
-    public string GetRoomRequirement()
-    {
-        if (Type == "Lecture") return "Великий зал";
-        if (Type == "Lab") return "Комп'ютерний клас";
-        return "Звичайна аудиторія";
-    }
+    public abstract string GetRoomRequirement();
 }
+
+public class Lecture : LessonType
+{
+    public override string GetRoomRequirement() => "Великий зал";
+}
+
+public class LaboratoryWork : LessonType
+{
+    public override string GetRoomRequirement() => "Комп'ютерний клас";
+}
+
